e9fa93f6f4be5a8c7114204424bfdc9ad5bf9ba2
IsraaAbdElnaby <israa.tobgy@gmail.com>
2018-06-10 18:34:07 +0200

    Front-end

--- Phase#2/src/App.js
+++ Phase#2/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './Page/Header';
 import TvMain from './Page/VedioTvMain';
 import Footer from './Page/Footer';
@@ -6,58 +7,47 @@ import BioMain from './Page/BioMain';
 import EventMain from './Page/events_main';
 import GuestsMain from './Page/Guests_main';
 import PhotosMain from './Page/Photos_main';
-import IntroMAin from'./Page/introMain';
+import IntroMAin from './Page/introMain';
 import PublicMAin from './Page/Public-Speaking';
 import MediaMain from './Page/mediaMain';
 import './Page/CSS/app_style.css'
 
 
 class App extends Component {
-  state={
-    Bio:{
-      title:'Biography',
-      main:<BioMain/>
-    },
-    Tv:{
-      title:'Tv Show',
-      main:<TvMain/>
-    },
-    Vedio:{
-      title:'Vedios',
-      main:<TvMain/>
-    },
-    Event:{
-      title:'Event',
-      main:<EventMain/>
-    },
-    Guest:{
-      title:'Guests',
-      main:<GuestsMain/>
-    },
-    Photo:{
-      title:'Photos',
-      main:<PhotosMain/>
-    },
-    Intro:{
-      title:'Hello!',
-      main:<IntroMAin/>
-    },
-    Public:{
-      title:'Public Speaking',
-      main:<PublicMAin/>
-    },
-    Media:{
-      title:'Media',
-      main:<MediaMain/>
-    },
-  };
 
   render() {
+
     return (
+
       <div className="container">
-        <Header>{this.state.Intro.title}</Header>
-        {this.state.Intro.main}
-        <Footer/>
+        <Router>
+          <Switch>
+            <Route exact path="/" render={() => <Header title="Hello!" />} />
+            <Route path="/events" render={() => <Header title="events" />} />
+            <Route path="/tvshows" render={() => <Header title="TV Shows" />} />
+            <Route path="/biography" render={() => <Header title="Biography" />} />
+            <Route path="/guests" render={() => <Header title="Our Guests" />} />
+            <Route path="/photos" render={() => <Header title="Photos" />} />
+            <Route path="/videos" render={() => <Header title="Videos" />} />
+            <Route path="/publicSpeaking" render={() => <Header title="Public Speaking" />} />
+            <Route path="/media" render={() => <Header title="Media Training" />} />
+          </Switch>
+        </Router>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={IntroMAin} />
+            <Route path='/events' component={EventMain} />
+            <Route path='/tvshows' component={TvMain} />
+            <Route path='/biography' component={BioMain} />
+            <Route path='/guests' component={GuestsMain} />
+            <Route path='/photos' component={PhotosMain} />
+            <Route path='/videos' component={TvMain} />
+            <Route path='/publicSpeaking' component={PublicMAin} />
+            <Route path='/media' component={MediaMain} />
+          </Switch>
+        </Router>
+
+        <Footer />
       </div>
     );
   }
--- Phase#2/src/Page/Header.js
+++ Phase#2/src/Page/Header.js
@@ -8,7 +8,7 @@ const header = (props) => {
     return (
         <header>
             <div><img src={require('./CSS/Vector-Smart-Object8.png')} className="logo" style={arrowStyle} /></div>
-            <div><p className="titel">{props.children}</p></div>
+            <div><p className="titel">{props.title}</p></div>
             <div ><img src={require('./CSS/Vector-Smart-Object9.png')} className="menue" style={arrowStyle} /></div>
         </header>
     );
