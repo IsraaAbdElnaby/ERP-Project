25b045f45b657dd942306093f876a21973192f76
abdoadel459 <40246539+abdoadel459@users.noreply.github.com>
2018-06-15 04:16:08 +0200

    Update mediaMain.js

--- Phase#2/src/Page/mediaMain.js
+++ Phase#2/src/Page/mediaMain.js
@@ -1,23 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './CSS/Media-Training-style.css';
-import Text from './mainText'
+import Text from './mainText';
 
-class media extends Component{
-    sendRequestHandler = () =>{
-        console.log("Request Sent");       
+class media extends Component {
+    state = {
+        pictures:{}
+    };
+    componentDidMount() {
+        this.callApi()
+            .then(res => {
+                this.setState({ pictures:res.Media})
+            })
+            .catch(err => console.log(err));
     }
-    render(){
-        return(
-            <main className = 'mediaMain'>
-                <div className = 'mediaImage'>
-                  <img src = {require('./CSS/Jessy el  murr.png')}/>
+
+    callApi = async () => {
+        const response = await fetch('/api');
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+        return body;
+    };
+    sendRequestHandler = () => {
+        alert("Request Sent");
+    }
+    render() {
+        return (
+            <main className='mediaMain'>
+                <div className='mediaImage'>
+                <img src={this.state.pictures.picture}/>
                 </div>
-                <div className = 'mediaMid'>
-                  <Text/>
-                  <button onClick = {this.sendRequestHandler}>Send Request</button>
+                <div className='mediaMid'>
+                    <Text />
+                    <button onClick={this.sendRequestHandler}>Send Request</button>
                 </div>
 
-            </main>    
+            </main>
         );
     }
 
